9b08dd1c7f827f786644e5d66ac387db31ad4fa2
Jonathan Dowland <jdowland@redhat.com>
2018-10-09 11:51:30 +0100

    use "rpm -q" to query local RPMs, not "yum list"…
    This is faster and does not require network access/activity

--- jboss/container/openjdk/jdk/11/configure.sh
+++ jboss/container/openjdk/jdk/11/configure.sh
@@ -3,15 +3,8 @@
 set -e
 
 # As of rhel 7.6, rh-maven35 pulls in jdk8, so we need to remove them
-
-if [ -n "$(yum list installed java-1.8.0-openjdk-devel |grep java-1.8.0-openjdk-devel)" ]; then
-    rpm -e --nodeps java-1.8.0-openjdk-devel
-fi
-
-if [ -n "$(yum list installed java-1.8.0-openjdk-headless |grep java-1.8.0-openjdk-headless)" ]; then
-    rpm -e --nodeps java-1.8.0-openjdk-headless
-fi
-
-if [ -n "$(yum list installed java-1.8.0-openjdk |grep java-1.8.0-openjdk)" ]; then
-    rpm -e --nodeps java-1.8.0-openjdk
-fi
+for pkg in java-1.8.0-openjdk-devel java-1.8.0-openjdk-headless java-1.8.0-openjdk; do
+    if rpm -q --quiet "$pkg"; then
+        rpm -e --nodeps "$pkg"
+    fi
+done
